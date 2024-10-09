af5aac9a54164c0090521632a189d12cf1d6ab3c
martywebdev <martywebdevelopment@gmail.com>
2024-10-09 09:32:50 +0800

    smooth scrolling

--- js/script.js
+++ js/script.js
@@ -17,6 +17,32 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector('.header')
 btnNavEl.addEventListener('click', ()=> headerEl.classList.toggle('nav-open'))
 
+// smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link')
+allLinks.forEach(el => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault()
+        const href = el.getAttribute('href')
+       
+        // scroll back to top
+        if(href === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
+        // scroll to other links
+        if (href !== '#' && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href)
+            sectionEl.scrollIntoView({behavior: "smooth"})
+        }
+        // close mobile navigation
+        if(el.classList.contains('main-nav-link')) {
+            headerEl.classList.toggle('nav-open')
+        }
+    })
+})
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
